2251da53b9486c68db262e382203f6d0449ccef2
Alexander <serowy@hotmail.com>
2020-02-08 06:48:16 +0100

    AB#13 added builder to dependency injection, deprecated AddSagas<>()

---
+++ src/TreeLine.Sagas.DependencyInjection/Configuration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace TreeLine.Sagas.DependencyInjection
+{
+    public interface IConfiguration
+    {
+        IServiceCollection Configure(IServiceCollection services);
+    }
+
+    internal sealed class Configuration : IConfiguration
+    {
+        private readonly IList<Action<IServiceCollection>> _configurations = new List<Action<IServiceCollection>>();
+
+        public IServiceCollection Configure(IServiceCollection services)
+        {
+            foreach (var configuration in _configurations)
+            {
+                configuration.Invoke(services);
+            }
+
+            return services;
+        }
+
+        internal void Add(Action<IServiceCollection> configuration)
+        {
+            _configurations.Add(configuration);
+        }
+    }
+}
