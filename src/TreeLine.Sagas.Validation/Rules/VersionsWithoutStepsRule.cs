e7bdfd1b02c78c3b1cb6503ea3a7c334fe766533
Alexander <serowy@hotmail.com>
2020-02-09 17:50:57 +0100

    ab#22 added validation for versions without steps

---
+++ src/TreeLine.Sagas.Validation/Rules/VersionsWithoutStepsRule.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using TreeLine.Sagas.Validation.Rules.Analyzing;
+
+namespace TreeLine.Sagas.Validation.Rules
+{
+    internal sealed class VersionsWithoutStepsRule : IValidationRule
+    {
+        private readonly ISagaProfileAnalyzerFactory _analyzerFactory;
+        private readonly IEnumerable<ISagaProfile> _profiles;
+        private readonly ILogger<VersionsWithoutStepsRule> _logger;
+
+        public VersionsWithoutStepsRule(
+            ISagaProfileAnalyzerFactory analyzerFactory,
+            IEnumerable<ISagaProfile> profiles,
+            ILogger<VersionsWithoutStepsRule> logger)
+        {
+            _analyzerFactory = analyzerFactory;
+            _profiles = profiles;
+            _logger = logger;
+        }
+
+        public void Validate()
+        {
+            foreach (var profile in _profiles)
+            {
+                var analyzer = _analyzerFactory.Create();
+                profile.Configure(analyzer);
+
+                if (analyzer.VersionAnalyzer.Any(anlyzr => anlyzr.StepCount.Equals(0)))
+                {
+                    _logger.LogWarning($"{profile.GetType().Name} has registered versions without configured steps.");
+                }
+            }
+        }
+    }
+}
