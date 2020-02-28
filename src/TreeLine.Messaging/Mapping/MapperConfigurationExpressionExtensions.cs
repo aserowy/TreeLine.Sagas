55db7ed2f0d68449ed592b187e1842eaeadb8962
Alexander <serowy@hotmail.com>
2020-02-28 07:11:34 +0100

    ab#21 added flat mapping

--- src/TreeLine.Messaging/Mapping/MapperConfigurationExpressionExtensions.cs
+++ src/TreeLine.Messaging/Mapping/MapperConfigurationExpressionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace TreeLine.Messaging.Mapping
 {
@@ -11,8 +12,46 @@ namespace TreeLine.Messaging.Mapping
                 .CreateMap(typeof(JObject), type.TargetType)
                 .IncludeBase(typeof(JObject), typeof(MessageBase));
 
+            var settableProperties = type
+                .TargetType
+                .GetProperties()
+                .Where(prprty => prprty.CanWrite);
+
+            //var customClassTypes = new List<Type>();
+            foreach (var propertyInfo in settableProperties)
+            {
+                RegisterForMember(ref map, propertyInfo.Name);
+
+                //if (!propertyInfo.PropertyType.Namespace.StartsWith("System", StringComparison.OrdinalIgnoreCase))
+                //{
+                //    customClassTypes.Add(propertyInfo.PropertyType);
+                //}
+            }
+
+            var settableFields = type
+                .TargetType
+                .GetFields();
+
+            foreach (var fieldInfo in settableFields)
+            {
+                RegisterForMember(ref map, fieldInfo.Name);
+            }
+
             // TODO: Create map froms for properties
             // TODO: Create maps for complex types nested in target type
+        }
+
+        private static void RegisterForMember(ref IMappingExpression map, string memberName)
+        {
+            map.ForMember(memberName, cnfgrtn => cnfgrtn.MapFrom((src, _) =>
+            {
+                if (!(src is JObject jObject))
+                {
+                    throw new AutoMapperMappingException($"{nameof(src)} is not of type {nameof(JObject)}.");
+                }
+
+                return jObject[memberName] ?? JToken.Parse("{}");
+            }));
         }
     }
 }
